4877a78939c56590dbbf0ee017fd882eeca06109
Chun-wei Kuo <Dendoh@gmail.com>
2013-12-13 22:23:23 +0800

    Accept command-line arguments

--- iloveck101.rkt
+++ iloveck101.rkt
@@ -4,9 +4,20 @@
          (planet neil/html-parsing:2:0)
          (planet lizorkin/sxml:2:1/sxml))
 
+(define verbose-mode (make-parameter #f))
+
+(define thread-url
+  (command-line
+   #:program "iloveck101"
+   #:once-each
+   [("--verbose" "-v") "Be verbose" (verbose-mode #t)]
+   #:ps "<url> should be a url of a thread, e.g. http://ck101.com/thread-2826444-1-1.html"
+   #:args
+   (url) (string->url url)))
+
 (define doc
   (call/input-url
-    (string->url "http://ck101.com/thread-2826444-1-1.html")
+    thread-url
     (lambda (url headers)
       (get-pure-port url #:redirections 5))
     (lambda (in)
@@ -14,10 +25,11 @@
     (list "User-Agent: Mozilla/5.0")))
 
 (define urls
-  ((sxpath "//img/@file/text()") doc))
+  (map string->url ((sxpath "//img/@file/text()") doc)))
 
 (for-each
   (lambda (a-url)
+    (and verbose-mode (printf "Downloading ~a...~n" (url->string a-url)))
     (copy-port
       (get-pure-port a-url)
       (open-output-file
